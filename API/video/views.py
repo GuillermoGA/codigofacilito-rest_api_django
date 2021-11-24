a217fb29dcd535e6a79d09263a0ad0b32e91a8c1
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-24 17:24:48 +0100

    serialize model

--- API/video/views.py
+++ API/video/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from video.models import Video
+from .serializers import VideoSerializer
 
 
 # Create your views here.
 class ListVideo(APIView):
     def get(self, request):
         videos = Video.objects.all()
-        return Response(videos)
+        video_json = VideoSerializer(videos, many=True)
+        return Response(video_json.data)
